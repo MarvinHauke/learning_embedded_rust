a119d15905ecf9d8d7570ce991129e8c6af76784
PfoRsten <marvin-hauke@hotmail.de>
2025-03-31 19:19:56 +0200

    runnign the programm with registers instead of dependencies

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,7 @@
 #![no_main]
 
 use core::panic::PanicInfo;
-use cortex_m::peripheral::syst::SystClkSource;
-use cortex_m::peripheral::Peripherals;
-use cortex_m::{asm, peripheral};
+use cortex_m::asm;
 use cortex_m_rt::{entry, exception};
 use cortex_m_semihosting::hprintln;
 
@@ -13,6 +11,14 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+//Registers:
+//System tick - control and status register:
+const SYST_CSR: *mut u32 = 0xE000_E010 as *mut u32;
+//System tick - reload value register:
+const SYST_RVR: *mut u32 = 0xE000_E014 as *mut u32;
+//System tick - current value register:
+const SYST_CVR: *mut u32 = 0xE000_E018 as *mut u32;
+
 // CPU frequency is 12.5MHz by default
 const CPU_FREQ: u32 = 12_500_000;
 
@@ -20,13 +26,16 @@ const CPU_FREQ: u32 = 12_500_000;
 fn main() -> ! {
     hprintln!("Starting program!");
 
-    let peripherals = Peripherals::take().unwrap();
-    let mut systick = peripherals.SYST;
-    systick.enable_interrupt();
-    systick.set_clock_source(SystClkSource::Core);
-    systick.set_reload(CPU_FREQ); // sets the delay frequency
-    systick.clear_current();
-    systick.enable_counter();
+    let sleep_dur: u32 = CPU_FREQ;
+
+    unsafe {
+        // Set the timer duration ( set delaytime to 1sec ):
+        *SYST_RVR = sleep_dur;
+        // Clear the current value by writing to the register:
+        *SYST_CVR = 0;
+        // Enable the timer:
+        *SYST_CSR = 0b111;
+    }
 
     loop {
         asm::wfi();
