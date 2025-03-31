2d4a761d62ace54b01d4d666454d2c41e09d25eb
PfoRsten <marvin-hauke@hotmail.de>
2025-03-31 19:53:43 +0200

    running the programm with timer configuration

--- src/main.rs
+++ src/main.rs
@@ -19,12 +19,29 @@ const SYST_RVR: *mut u32 = 0xE000_E014 as *mut u32;
 //System tick - current value register:
 const SYST_CVR: *mut u32 = 0xE000_E018 as *mut u32;
 
+// Configuration of Timers:
 // CPU frequency is 12.5MHz by default
 const CPU_FREQ: u32 = 12_500_000;
+
+// Run-Mode clock Configuration (RCC):
+const RCC: u32 = 0x400FE060;
+// Prescaler value:
+const SYSCTL_SYSDIV_12: u32 = 0xB; // Check Datasheet for other prescaler values
+const SYSCTL_SYSDIV_16: u32 = 0xF; //Different Prescaler value just change "SYSCTL_SYSDIV_12" to "SYSCTL_SYSDIV_16"
 
 #[entry]
 fn main() -> ! {
     hprintln!("Starting program!");
+
+    //Set Prescaler value for RCC
+    unsafe {
+        let sysdiv = SYSCTL_SYSDIV_12 << 23; // change here for running faster or slower interrupts. at the
+                                             // moment the interrupt is triggering slightly faster then a second with SYSCTL_SYSDIV_12
+        let orig = *(RCC as *const u32);
+        let mask: u32 = !0b1111 << 23;
+        let rcc = (orig & mask) | sysdiv;
+        *(RCC as *mut u32) = rcc;
+    }
 
     let sleep_dur: u32 = CPU_FREQ;
 
